3cc3a95127ef58efbc305d9b366dc8b04dd655a2
Gabriel <gabrielja2antunes@gmail.com>
2022-09-21 13:44:39 -0300

    feat: realizando a chamada a api com fetch

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-const URL = 'https://pokeapi.co/api/v2/pokemon?limit=18&offset=0';
+// const URL = 'https://pokeapi.co/api/v2/pokemon?limit=18&offset=0';
 
-// fetch API
-const useFetch = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState([]);
+// // fetch API
+// const useFetch = () => {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [data, setData] = useState([]);
   
   
-  useEffect(() => {
-    setIsLoading(true);
-    const fetchData = async () => {
-      const allPokemons = await fetch(URL);
-      const json = await allPokemons.json();
+//   useEffect(() => {
+//     setIsLoading(true);
+//     const fetchData = async () => {
+//       const allPokemons = await fetch(URL);
+//       const json = await allPokemons.json();
 
 
-      setData(json.results); 
-      setIsLoading(false); 
-    };
-    fetchData();
-  }, []);
+//       setData(json.results); 
+//       setIsLoading(false); 
+//     };
+//     fetchData();
+//   }, []);
   
-  return { isLoading, data };
-};
+//   return { isLoading, data };
+// };
 
 
-export default useFetch;
+// export default useFetch;
