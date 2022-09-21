a93b14b01bd669ac112a3efae7edd16cdbb38c8d
Gabriel <gabrielja2antunes@gmail.com>
2022-09-21 13:43:10 -0300

    feat: criando index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Provider from './context/provider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <Provider>
     <App />
-  </>
+  </Provider>
 );
 
 
